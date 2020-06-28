3207a25213b9d9c30fd7e66179a4b28bc28ea954
Brooks Patton <brooks@brookzerker.com>
2020-06-28 12:13:40 -0600

    created the arena that the game will be played in

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+let waterColor;
+let wallColor;
+let worldUnitSize;
+
 function setup() {
   createCanvas(1920, 1080);
+
+  waterColor = color("#44a1d3");
+  wallColor = color("brown");
+  worldUnitSize = 10;
 }
 
 function draw() {
-  background(0);
+  drawBackground();
+}
+
+function drawBackground() {
+  fill(waterColor);
+  rect(0, height - worldUnitSize, width, worldUnitSize);
+  fill(wallColor);
+  rect(0, 0, worldUnitSize, height);
+  rect(width - worldUnitSize, 0, worldUnitSize, height);
+  rect(0, 0, width, worldUnitSize);
 }
